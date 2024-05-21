a5d9c481dc8dd0fad8e28640df0a429076d71740
Gulshan <shrigulshankumar@gmail.com>
2024-05-21 19:16:37 +0300

    Added json response and football api

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
+import os
+import requests
 
 def index(request):
-    return HttpResponse("Hello world")
+    # today = datetime.today().strftime('%y-%m-%d')
+    # url = os.environ.get('URLAPI')
+    url = os.environ.get('URLAPILIVE')
+    response = requests.get(url)
+    jsonResponse = response.json()
+    return render(request,'blog/index.html', {"jsonResponse" : jsonResponse})
